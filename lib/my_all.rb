8e5731ec5697e96c47bf583e2c4e7e60b0e5bfac
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-04-24 17:34:26 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,8 +2,9 @@ require 'pry'
 
 def my_all?(collection)
   i = 0
+  block_return_values = []
   while i < collection.length
-    yield(collention[i])
+    block_return_values << yield(collention[i])
     i = i + 1
   end
 end
